8f2e0e2cc523a247fc6a84606c4607e1a009f13e
170030679 <59110806+170030679@users.noreply.github.com>
2020-01-04 15:08:36 +0530

    Update ReverseString.java

--- ReverseString.java
+++ ReverseString.java
@@ -1,24 +1,24 @@
-
-import java.util.Scanner;
- 
-
-public class ReverseString
-{
-    public static void main(String[] args)
-    {
-        System.out.println("Enter string to reverse:");
-        
-        Scanner read = new Scanner(System.in);
-        String str = read.nextLine();
-        String reverse = "";
-        
-        
-        for(int i = str.length() - 1; i >= 0; i--)
-        {
-            reverse = reverse + str.charAt(i);
-        }
-        
-        System.out.println("Reversed string is:");
-        System.out.println(reverse);
-    }
-}
+// Java program to ReverseString using StringBuilder 
+import java.lang.*; 
+import java.io.*; 
+import java.util.*; 
+  
+// Class of ReverseString 
+class ReverseString 
+{ 
+    public static void main(String[] args) 
+    { 
+        String input = "Geeks for Geeks"; 
+  
+        StringBuilder input1 = new StringBuilder(); 
+  
+        // append a string into StringBuilder input1 
+        input1.append(input); 
+  
+        // reverse StringBuilder input1 
+        input1 = input1.reverse(); 
+  
+        // print reversed String 
+        System.out.println(input1); 
+    } 
+} 
